08d9fd45ff86cec5bfea8b419413010e5a57ad80
José Henrique <josehpaludo@gmail.com>
2023-06-16 00:16:46 -0300

    commit inicial

---
+++ entities/pessoas.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    //construtor da classe pessoa
+    constructor(codigo, nome, cidade, uf) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+}
+
+module.exports = Pessoa;
---
+++ entities/tarefas.js
@@ -0,0 +1,11 @@
+class Tarefa {
+    constructor(codigo, titulo, descricao, pessoa, nomepessoa) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.pessoa = pessoa;
+        this.nomepessoa = nomepessoa;       
+    }
+}
+
+module.exports = Tarefa;
